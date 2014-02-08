34374767c87d8e39939e348a82eca34ab5df90f4
Joel Sommers <jsommers@acm.org>
2014-02-08 12:40:17 -0500

    Initial commit

---
+++ myrouter1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+'''
+Basic IPv4 router (static routing) in Python, stage 1.
+'''
+
+import sys
+import os
+import os.path
+sys.path.append(os.path.join(os.environ['HOME'],'pox'))
+sys.path.append(os.path.join(os.getcwd(),'pox'))
+import pox.lib.packet as pktlib
+from pox.lib.packet import ethernet,ETHER_BROADCAST,IP_ANY
+from pox.lib.packet import arp
+from pox.lib.addresses import EthAddr,IPAddr
+from srpy_common import log_info, log_debug, log_warn, SrpyShutdown, SrpyNoPackets
+
+class Router(object):
+    def __init__(self, net):
+        self.net = net
+
+    def router_main(self):    
+        while True:
+            try:
+                dev,ts,pkt = self.net.recv_packet(timeout=1.0)
+            except SrpyNoPackets:
+                # log_debug("Timeout waiting for packets")
+                continue
+            except SrpyShutdown:
+                return
+
+
+def srpy_main(net):
+    '''
+    Main entry point for router.  Just create Router
+    object and get it going.
+    '''
+    r = Router(net)
+    r.router_main()
+    net.shutdown()
+    

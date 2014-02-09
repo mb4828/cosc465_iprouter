6020f4b3f07f614ec622d135be12f02d78584cff
Joel Sommers <jsommers@acm.org>
2014-02-08 21:48:59 -0500

    Add debugger import in template file

--- myrouter1.py
+++ myrouter1.py
@@ -13,7 +13,7 @@ import pox.lib.packet as pktlib
 from pox.lib.packet import ethernet,ETHER_BROADCAST,IP_ANY
 from pox.lib.packet import arp
 from pox.lib.addresses import EthAddr,IPAddr
-from srpy_common import log_info, log_debug, log_warn, SrpyShutdown, SrpyNoPackets
+from srpy_common import log_info, log_debug, log_warn, SrpyShutdown, SrpyNoPackets, debugger
 
 class Router(object):
     def __init__(self, net):
